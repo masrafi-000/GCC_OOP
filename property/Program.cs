331ed76361369ab26cccd0224eb8125d34b457e7
masrafi-000 <smmasrafi000@gmail.com>
2026-06-01 17:24:00 +0000

    add initial implementation of Person, Vehicle, and Car classes with project file

---
+++ property/Program.cs
@@ -0,0 +1,39 @@
+﻿
+class Person
+{
+    public string Name
+    {
+      get; set;
+    }
+}
+
+class Vehicle
+{
+    public string brand = "Ford";
+    public void honk()
+    {
+        Console.WriteLine("Tuut, tuut!");
+    }
+}
+
+class Car : Vehicle
+{
+    public string modelName = "Mustang";
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person Obj = new Person();
+        Obj.Name = "Masrafi";
+        Console.WriteLine(Obj.Name);
+
+        Car newCar = new Car();
+
+        newCar.honk();
+
+        Console.WriteLine(newCar.brand + " " + newCar.modelName);
+    }
+}
