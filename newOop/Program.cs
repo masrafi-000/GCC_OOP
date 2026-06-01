9a82215c085081cfbf644f1dec49b0d7d8f63e83
masrafi-000 <smmasrafi000@gmail.com>
2026-06-01 16:59:44 +0000

    migrate project structure to newOop directory and add initial implementation

---
+++ newOop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] input    = Console.ReadLine().Split();
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n ; i++)
+        {
+            arr[i] = int.Parse(input[i]);
+        }
+
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+
+        foreach(int num in arr)
+        {
+            freq[num] = freq.GetValueOrDefault(num) +1;
+        }
+
+        foreach(var items in freq)
+        {
+            Console.WriteLine($"{items.Key} -> {items.Value}");
+        }
+
+    }
+}
