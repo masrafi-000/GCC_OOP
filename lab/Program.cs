990fb9a369efa1a7d2bfdf3d556d7747fc4a302f
masrafi-000 <smmasrafi000@gmail.com>
2026-06-13 16:18:47 +0000

    add initial implementation of Program class and project file

---
+++ lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       int n;
+
+       Console.WriteLine("Enter a number: ");
+       n = Convert.ToInt32(Console.ReadLine());
+
+       if(n % 2 == 0)
+        {
+            Console.WriteLine(n + "is even");
+        } else
+        {
+            Console.WriteLine(n + "is odd");
+            
+        }
+    }
+}
